01647701de7bac03fb5f2af7d522e9882d237a7d
Clockwork <alex@clockwork.gr>
2021-10-09 00:40:59 +0300

    Complete boilerplate setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,14 @@
 module.exports = {
-	root: true,
-	env: {
-		node: true,
-		browser: true,
-		es6: true 
-	},
-	plugins: ['prettier'],
-	extends: [
-		'eslint:recommended',
-		"plugin:prettier/recommended",
-	],
-	
-		parser: '@babel/eslint-parser',
-	
-	rules: {
-		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-	}
-}
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  parserOptions: {
+    parser: "@babel/eslint-parser",
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
+};
